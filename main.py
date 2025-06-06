e90a57386f5e2696281c6064924e77c2665357c4
Vany Popov <vanypopov107@gmail.com>
2025-06-06 14:49:03 +0300

    add working sample

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode([500, 500])
+WIDTH, HEIGHT = 500, 500
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+body_width: int = 50
+body_height: int = 50
+
 x: int = 250
-y: int = 427
-speed: int = 5
+y: int = HEIGHT-body_height//2
+
+speed: int = 10
 is_jump: bool = False
-jump_count: int = 10
+jump_height: int = 10
 
 left: bool = False
 right: bool = False
 
-width: int = 50
-height: int = 50
-
 clock = pygame.time.Clock()
 
 while True:
-    clock.tick(30)
+    screen.fill((255, 255, 255))
+    pygame.draw.circle(screen, (255, 0, 0), (x, y), 25, 25)
+
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             exit()
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and x > -7:
-        x -= speed
-    elif keys[pygame.K_RIGHT] and x < 450:
-        x += speed
+    if keys[pygame.K_LEFT] and x > body_width//2:
+        if x - speed < body_width//2:
+            x = body_width//2
+        else:
+            x -= speed
+
+    elif keys[pygame.K_RIGHT] and x < WIDTH-body_width//2:
+        if x + speed > WIDTH-body_width // 2:
+            x += WIDTH-body_width//2
+        else:
+            x += speed
+
+
+    pygame.display.flip()
+    clock.tick(60)
